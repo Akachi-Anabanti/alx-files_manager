aabd71a2e121c45ca7b318df5967d61dd4ffb804
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-08-06 23:06:50 +0000

    mongo db client config

---
+++ utils/db.js
@@ -0,0 +1,29 @@
+// Mongo DB setup
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    const DB_HOST = process.env.DB_HOST || 'localhost';
+    const DB_PORT = process.env.DB_PORT || 27017;
+    const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.client.connect();
+  }
+
+  isAlive() {
+    return this.client.topology.isConnected();
+  }
+
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
