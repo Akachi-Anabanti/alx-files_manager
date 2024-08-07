9c7926aefb091d86e363489a89f7b14fc079fe26
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-08-07 15:01:59 +0000

    route updates

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,17 @@
 //
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 function routes(API) {
   API.get('/status', AppController.getStatus);
   API.get('/stats', AppController.getStats);
 
   API.post('/users', UsersController.postNew);
+  API.get('/users/me', UsersController.getMe);
+
+  API.get('/connect', AuthController.getConnect);
+  API.get('/disconnect', AuthController.getDisconnect);
 }
 
 export default routes;
