3a6cbdd42d45a1442714189f40e17351878f2013
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-08-07 00:25:09 +0000

    app listen, arg fix and import updates

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 // server file
 //
-const express = require('express');
-
+import express from 'express';
+import routes from './routes';
 
 const app = express();
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
 
+routes(app);
 
-app.listen((PORT) => {});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
