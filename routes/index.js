2988ba5eae9dadc0cc8aa2efef502d4ffdab6eca
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-08-07 01:29:04 +0000

    user creation route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 // entry point for routes
 //
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 function routes(API) {
   API.get('/status', AppController.getStatus);
   API.get('/stats', AppController.getStats);
+
+  API.post('/users', UsersController.postNew);
+
 }
 
 export default routes;
