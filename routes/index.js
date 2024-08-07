64e19f0086511842013778d5f6cd2809cd2486aa
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-08-07 00:24:48 +0000

    fix import statement

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 // entry point for routes
-
+//
+import AppController from '../controllers/AppController';
 
 function routes(API){
   API.get('/status', AppController.getStatus);
   API.get('/stats', AppController.getStats);
 }
+
+export default routes;
