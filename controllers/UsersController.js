35924a7e4424154f6b1c2e50510bd4236766eb82
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-08-07 15:01:40 +0000

    get user function

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,7 @@
+const { ObjectId } = require('mongodb');
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
+import { getTokenUserId } from '../utils/auth';
 
 export default class UserController {
   static async postNew(req, res) {
@@ -28,6 +30,29 @@ export default class UserController {
     return res.status(201).json({
       email,
       id: user.insertedId,
+    });
+  }
+
+  static async getMe(req, res) {
+    const token = req.header('X-TOKEN');
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const userId = await getTokenUserId(token);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const usersCollection = await dbClient.usersCollection();
+    const user = await usersCollection.findOne({ _id: new ObjectId(userId) });
+
+    if(!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({
+      email: user.email,
+      id: user._id.toString()
     });
   }
 }
